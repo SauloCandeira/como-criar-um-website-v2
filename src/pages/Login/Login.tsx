6c7b49d12c87d012dbf05307de34ad4a874422ae
saulo candeira <saulocandeira@saulos-MacBook-Pro.local>
2024-09-21 22:38:19 -0300

    add : new features 2

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithPopup, onAuthStateChanged } from "firebase/auth"; 
 import { auth, provider } from '../../lib/init-firebase'; 
